07aa92179f3032735f284359c464a59abaf42c81
timothy <timothyabiodun300@gmail.com>
2021-01-12 22:22:27 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flash_chat_app/screens/chat_screen.dart';
+import 'package:flash_chat_app/screens/login_screen.dart';
+import 'package:flash_chat_app/screens/registration_screen.dart';
+import 'package:flash_chat_app/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: 'home',
+      routes: {
+        'home': (context) => WelcomeScreen(),
+        'login': (context) => LoginScreen(),
+        'registration': (context) => RegistrationScreen(),
+        'chat_screen': (context) => ChatScreen()
+      },
+    );
+  }
+}
